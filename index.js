1538a04261e7884cbde90e656e71feda28e4df94
Vasylchenko Anton <87824792+AntonVasylchenko@users.noreply.github.com>
2024-08-17 12:18:56 +0300

    Changed

--- index.js
+++ index.js
@@ -10,45 +10,53 @@ app.use(cors());
 // Дозволяйте обробку JSON тіла запиту
 app.use(express.json());
 
-function queryApi() {   
-    console.log(123);
+function queryApi() {
+  console.log(123);
 
-    return new Promise(( resolve,reject) => {
-      fetch("https://learn123456.myshopify.com/apps/proxytest", {
-        method: "POST",
-        redirect: "manual",
-        headers: {
-          "Content-Type": "application/json",
-          "'Access-Control-Allow-Origin": "*"
-        }
-      }).then( response => {
-        console.log(response, "response");
-      }).then( response => {
-        resolve(response)
-      }).catch( error => {
-        console.log(error);
-      })
+  return new Promise((resolve, reject) => {
+    fetch("https://learn123456.myshopify.com/apps/proxytest", {
+      method: "POST",
+      redirect: "manual",
+      headers: {
+        "Content-Type": "application/json",
+        "'Access-Control-Allow-Origin": "*"
+      }
+    }).then(response => {
+      console.log(response, "response");
+    }).then(response => {
+      resolve(response)
+    }).catch(error => {
+      console.log(error);
     })
+  })
 }
 
 // Створіть POST endpoint
-app.post('/post-endpoint', (req, res) => {
-    const data = req;
-    console.log(req);
+app.post('/post-endpoint', async (req, res) => {
+  console.log("End point");
+  
+  try {
     res.json({
-        message: 'Дані отримані успішно!',
-        receivedData: data
+      message: 'Дані отримані успішно!'
     });
 
-    queryApi()()
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "error"
+    });
+
+  }
+
+
 });
 
 // Додайте GET endpoint для перевірки
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 // Запустіть сервер
 app.listen(port, () => {
-    console.log(`Сервер запущений на http://localhost:${port}`);
+  console.log(`Сервер запущений на http://localhost:${port}`);
 });
